4fb06fd5e42c01b43b2eaae88f91207338017d92
Ian Travers <mr.itpe@gmail.com>
2022-02-18 10:29:57 +0300

    Fix Vue after breeze installing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 require('./bootstrap');
 
 import Alpine from 'alpinejs';
+import { createApp} from "vue";
+import Home from "./components/Home";
+
+const app = createApp({
+    components: { Home }
+});
+
+app.mount("#app");
 
 window.Alpine = Alpine;
-
 Alpine.start();

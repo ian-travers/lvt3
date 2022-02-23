09ac1ec4aa2daac0962033453adb1b06c054887b
Ian Travers <mr.itpe@gmail.com>
2022-02-23 14:13:52 +0300

    Update JustGetJson to composition api

--- resources/js/components/JustGetJson.vue
+++ resources/js/components/JustGetJson.vue
@@ -1,21 +1,28 @@
 <template>
     <ul>
-        <li v-for="skill in skills">{{ skill}}</li>
+        <li v-for="skill in allSkills.skills">{{ skill }}</li>
     </ul>
 </template>
 
 <script>
+import {onMounted, reactive} from 'vue'
 export default {
-    data() {
+    setup() {
+        const allSkills = reactive({
+            skills: [],
+        })
+
+        onMounted(() => {
+            console.log('Here is m')
+            axios
+                .get('/skills')
+                .then(response => allSkills.skills = response.data)
+        })
+
         return {
-            skills: []
+            allSkills
         }
     },
-    mounted() {
-        axios
-            .get('/skills')
-            .then(response => this.skills = response.data)
-    }
 }
 </script>
 

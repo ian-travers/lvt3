f1d6735bfadf60c2a79eddbe3b4d609972cfdfd4
Ian Travers <mr.itpe@gmail.com>
2022-02-24 15:53:48 +0300

    Ep.20 Vue2 step by step is done (OOP form v.2) VI

--- resources/js/components/ProjectForm.vue
+++ resources/js/components/ProjectForm.vue
@@ -1,37 +1,37 @@
 <template>
-    <div class="space-y-4" @keydown="errors.clear($event.target.name)">
+    <div class="space-y-4" @keydown="form.errors.clear($event.target.name)">
         <div>
             <div
                 class="border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
                 <label for="name" class="block text-xs font-medium text-gray-900">Name</label>
                 <input
-                    v-model="name"
+                    v-model="form.name"
                     type="text"
                     name="name"
                     id="name"
                     class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
                     placeholder="Project name"/>
             </div>
-            <p v-if="errors.has('name')" v-text="errors.get('name')" class="mt-1 text-xs text-red-600"></p>
+            <p v-if="form.errors.has('name')" v-text="form.errors.get('name')" class="mt-1 text-xs text-red-600"></p>
         </div>
         <div>
             <div
                 class="border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
                 <label for="name" class="block text-xs font-medium text-gray-900">Description</label>
                 <input
-                    v-model="description"
+                    v-model="form.description"
                     type="text"
                     name="description"
                     id="description"
                     class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
                     placeholder="Project brief description"/>
             </div>
-            <p v-if="errors.has('description')" v-text="errors.get('description')" class="mt-1 text-xs text-red-600"></p>
+            <p v-if="form.errors.has('description')" v-text="form.errors.get('description')" class="mt-1 text-xs text-red-600"></p>
         </div>
 
         <button
             @click.prevent="onSubmit"
-            :disabled="errors.any()"
+            :disabled="form.errors.any()"
             type="button"
             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50"
         >
@@ -65,33 +65,75 @@ class Errors {
     }
 
     clear(field) {
-        delete this.errors[field]
+        if (field) {
+            delete this.errors[field]
+
+            return
+        }
+
+        this.errors = {}
+    }
+}
+
+class Form {
+    constructor(data) {
+        this.originalData = data
+
+        for (let field in data) {
+            this[field] = data[field]
+        }
+
+        this.errors = new Errors()
+    }
+
+    data() {
+        // Clone
+        let data = Object.assign({}, this)
+
+        delete data.originalData
+        delete data.errors
+
+        return data
+    }
+
+    reset() {
+        for (let field in this.originalData) {
+            this[field] = ''
+        }
+    }
+
+    submit(requestType, url) {
+        axios[requestType](url, this.data())
+            .then(this.onSuccess.bind(this))
+            .catch(this.onFail.bind(this))
+    }
+
+    onSuccess(response) {
+        this.reset()
+        this.errors.clear()
+
+        alert(response.data.message)
+    }
+
+    onFail(error) {
+        this.errors.record(error.response.data.errors)
     }
 }
 
 export default {
     data() {
         return {
-            name: '',
-            description: '',
-            errors: new Errors(),
+            form: new Form({
+                name: '',
+                description: ''
+            })
         }
     },
 
     methods: {
         onSubmit() {
-            axios.post('/projects', this.$data)
-                .then(this.onSuccess)
-                .catch(error => this.errors.record(error.response.data.errors))
-        },
-
-        onSuccess(response) {
-            alert(response.data.message)
-            this.name = ''
-            this.description = ''
+            this.form.submit('post', '/projects')
         }
-    },
-
-
+    }
 }
 </script>

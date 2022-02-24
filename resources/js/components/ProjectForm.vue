d42314c2ca9281b936a0975ae4891bdfee2ca31c
Ian Travers <mr.itpe@gmail.com>
2022-02-24 13:16:34 +0300

    Disable submit button

--- resources/js/components/ProjectForm.vue
+++ resources/js/components/ProjectForm.vue
@@ -31,8 +31,9 @@
 
         <button
             @click.prevent="onSubmit"
+            :disabled="errors.any()"
             type="button"
-            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50"
         >
             Create
         </button>
@@ -47,6 +48,10 @@ class Errors {
 
     has(field) {
         return this.errors.hasOwnProperty(field)
+    }
+
+    any() {
+        return Object.keys(this.errors).length > 0;
     }
 
     get(field) {

ce8cf969158cb8b36479c049f632ec6790739ac7
Ian Travers <mr.itpe@gmail.com>
2022-03-01 11:29:53 +0300

    Ep. 34 Start with flickity carousel

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,11 @@ import Home from "./components/Home"
 import Modal1 from "./components/Modal1"
 import JustGetJson from "./components/JustGetJson"
 import ProjectForm from "./components/ProjectForm"
-import MenuList from "./components/MenuList";
+import MenuList from "./components/MenuList"
+import Carousel from "./components/Carousel"
 
 const app = createApp({
-    components: { Home, Modal1, JustGetJson, ProjectForm, MenuList }
+    components: { Home, Modal1, JustGetJson, ProjectForm, MenuList, Carousel }
 });
 
 app.mount("#app");
---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="main-carousel" data-flickity='{ "cellAlign": "left", "contain": true, "wrapAround": true}'>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import Flickity from 'flickity'
+import 'flickity/dist/flickity.min.css'
+
+export default {
+}
+</script>

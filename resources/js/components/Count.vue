039d5ce0ef9681763821aac017604870b946ca02
Ian Travers <mr.itpe@gmail.com>
2022-03-14 11:53:12 +0300

    Use syntactic sugar for script statement

--- resources/js/components/Count.vue
+++ resources/js/components/Count.vue
@@ -2,37 +2,30 @@
     <span v-text="count" ref="num"></span>
 </template>
 
-<script>
+<script setup>
 import InView from 'in-viewport'
 import {computed, onMounted, ref} from "vue";
 
-export default {
-    props: ['to', 'velocity'],
+const props = defineProps(['to', 'velocity'])
+const num = ref(null)
+const count = ref(0)
+const interval = ref(0)
 
-    setup(props) {
-        const num = ref(null)
-        const count = ref(0)
-        const interval = ref(0)
+const increment = computed(() => Math.ceil(props.to / props.velocity))
 
-        const increment = computed(() => Math.ceil(props.to / props.velocity))
+function tick() {
+    if (count.value + increment.value >= props.to) {
+        count.value = props.to
 
-        function tick() {
-            if (count.value + increment.value >= props.to) {
-                count.value = props.to
+        return clearInterval(interval.value)
+    }
 
-                return clearInterval(interval.value)
-            }
-
-            count.value += increment.value
-        }
-
-        onMounted(() => {
-            InView(num.value, () => {
-                interval.value = setInterval(tick, props.velocity)
-            })
-        })
-
-        return {num, count, interval, increment, tick}
-    },
+    count.value += increment.value
 }
+
+onMounted(() => {
+    InView(num.value, () => {
+        interval.value = setInterval(tick, props.velocity)
+    })
+})
 </script>
